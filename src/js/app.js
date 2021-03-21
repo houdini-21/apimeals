f3872640192a15b66dcc8676cc7ac37e717f6e19
houdini21 <marinerocruzfer@gmail.com>
2021-03-21 17:55:24 -0600

    :sparkles: Now render from api

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,66 @@
-let hola = 1;
+async function getCategorySelected(category) {
+  const url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=';
+  const urlfinal = url + category;
+  try {
+    const res = await fetch(urlfinal);
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-hola = 3;
+async function getCategory() {
+  const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
+  try {
+    const res = await fetch(url);
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-console.log(hola);
+async function renderMealsCategory(category) {
+  const res = await getCategorySelected(category);
+  res.meals.forEach((meal) => {
+    const cardHtml = `
+          <div class="principal__card-item shadow">
+              <img src="${meal.strMealThumb}" alt="" class="principal__card-img-meals">
+          <div class="principal__card-item-back">
+            <p class="principal__card-item-p">${meal.strMeal}</p>
+          </div>
+          </div>
+
+        `;
+
+    document
+      .getElementById('principal-container')
+      .insertAdjacentHTML('beforeend', cardHtml);
+  });
+}
+
+async function renderCategories() {
+  const res = await getCategory();
+  res.categories.forEach((category) => {
+    const cardHtml = `
+          <div class="principal__card-item shadow" id="${category.strCategory}">
+              <img src="${category.strCategoryThumb}" alt="" class="principal__card-img">
+          <div class="principal__card-item-back">
+            <p class="principal__card-item-p">${category.strCategory}</p>
+          </div>
+          </div>
+
+        `;
+
+    document
+      .getElementById('principal-container')
+      .insertAdjacentHTML('beforeend', cardHtml);
+  });
+
+  const cards = document.querySelectorAll('.principal__card-item');
+  cards.forEach((card) => {
+    card.addEventListener('click', () => {
+      renderMealsCategory(card.id);
+    });
+  });
+}
+renderCategories();
